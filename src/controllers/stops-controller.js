27c4db1e0d05ca793459d87c5d2566a3946012b1
Brandon Danis <bdani093@uottawa.ca>
2018-03-19 19:04:46 -0400

    Refactoring and fixed distance when getting stops

--- src/controllers/stops-controller.js
+++ src/controllers/stops-controller.js
@@ -1,20 +1,43 @@
 const stopsModel = require("../models/stops-model");
 
-module.exports.getAllStops = async (req, res) => {
-    if(req.query.lat && req.query.lon){
-        stopsModel.selectAllStopsWithDistance(req.query.lat, req.query.lon, (err, stops) => {
-            res.status(err ? 500 : 200).json(err ? undefined : stops);
-        });
-    }
-    else{
-        stopsModel.selectAllStops((err, stops) => {
-            res.status(err ? 500 : 200).json(err ? undefined : stops);
-        });
+module.exports.getAllStops = async ({query: {lat = null, lon = null}}, res) => {
+    try {
+        var stops = await stopsModel.selectAllStops();
+        if (lat && lon) {
+            stops = stops.map((stop) => {
+                stop.distance = getDistanceFromLatLonInKm(lat, lon, stop.stop_lat, stop.stop_lon)
+                return stop
+            })
+        }
+        res.status(200).json(stops);
+    } catch (error) {
+        return res.status(500).json({error});
     }
 };
 
 module.exports.getStop = async (req, res) => {
-    stopsModel.selectStop(req.params.stopCode, (err, stop) => {
-        res.status(err ? 500 : 200).json(err ? undefined : stop);
-    });
+    if (!req.params.stopCode) {
+        res.status(400).json({error: "Missing stopCode parameter"})
+    }
+
+    try {
+        const stop = await stopsModel.selectStop(req.params.stopCode);
+        res.status(200).json(stop);
+    } catch (error) {
+        res.status(500).json({error});
+    }
 };
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
--- src/models/stops-model.js
+++ src/models/stops-model.js
@@ -1,58 +1,9 @@
 const db = require('pg-bricks').configure(process.env.DATABASE_URL || `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@localhost:5432/routes`);
 
-module.exports.selectAllStops = async (callback) => {
-    try {
-        let stops = await db.select().from('stops').rows();
-        callback(null, stops);
-    }
-    catch(e){
-        callback(e, null);
-    }
+module.exports.selectAllStops = () => {
+    return db.select().from('stops').rows();
 }
 
-module.exports.selectAllStopsWithDistance = async (lon, lat, callback) => {
-    try {
-        let stops = await db.select().from('stops').rows();
-        //let stopsWithDistance = [];
-
-        for(stop in stops){
-            console.log(stop)
-            stop.distance = calculateDistance(stop.stop_lat, stop.stop_lon, lon, lat);
-            console.log(stop)
-        }
-
-        callback(null, stops);
-    }
-    catch(e){
-        callback(e, null);
-    }
-}
-
-module.exports.selectStop = async (stopCode, callback) => {
-    try {
-        if(req.stopCode){
-            let stop = await db.select().from('stops').where({stop_code: stopCode}).row();
-            callback(null, stop);
-        }
-        callback(null, null);
-    }
-    catch(e){
-        callback(e, null);
-    }
-}
-
-// Source https://www.movable-type.co.uk/scripts/latlong.html
-function calculateDistance(lat1, lon1, lat2, lon2){
-    let R = 6371;
-    let φ1 = lat1.toRadians();
-    let φ2 = lat2.toRadians();
-    let Δφ = (lat2-lat1).toRadians();
-    let Δλ = (lon2-lon1).toRadians();
-
-    let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-    let d = R * c;
-
-    return Math.round(d * 100) / 100;
+module.exports.selectStop = (stopCode, callback) => {
+    return db.select().from('stops').where({stop_code: stopCode}).row();
 }
